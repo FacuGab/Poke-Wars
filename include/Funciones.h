d6eea0b0ba380ab486de9c7c9e79e88b3fdc7e42
FacuGabriel <76824041+FacuGab@users.noreply.github.com>
2021-11-12 18:09:01 -0300

    Add files via upload

---
+++ include/Funciones.h
@@ -0,0 +1,10 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+#include <windows.h>
+
+
+void gotoxy(short px, short py);
+
+void cuadro(int coluna1, int columna2, int fila1, int fila2);
+
+#endif // FUNCIONES_H
---
+++ src/Funciones.cpp
@@ -0,0 +1,27 @@
+//Nombre:
+#include <iostream>
+using namespace std;
+#include "Funciones.h"
+
+void gotoxy(short px, short py){
+    COORD pos;
+    pos.X = px;
+    pos.Y = py;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+void cuadro(int coluna1, int columna2, int fila1, int fila2){
+
+    for(int col = coluna1; col <= columna2; col++)
+    {
+        for(int fil = fila1; fil <= fila2; fil++)
+        {
+            gotoxy(col, fil);
+            if((col == coluna1 || col == columna2) && (fil != fila1 || fil != fila2)){
+                cout << "|";
+            }else if((fil == fila1 || fil == fila2) && (col != coluna1 || col != columna2)){
+                cout << "--";
+            }
+        }
+    }
+}
